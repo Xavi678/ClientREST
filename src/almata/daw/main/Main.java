ac3e74473bbf75baa11878a72c50e88ed6e66e18
Xavi678 <xavisp6@gmail.com>
2019-04-23 12:52:48 +0200

    h

--- src/almata/daw/main/Main.java
+++ src/almata/daw/main/Main.java
@@ -11,6 +11,7 @@ import javax.ws.rs.core.MediaType;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
@@ -24,56 +25,7 @@ public class Main {
 
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-		/*ObjectMapper mapper= new ObjectMapper();
-		Producte producte= new Producte();
-		//mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		System.out.println("Autenticar");
-		Token token=mapper.readValue(new URL("http://localhost:8080/ServeiREST/api/serveiCarro/Autenticar/tito/tito"), Token.class);
-	
-		System.out.println(token.getId());
 		
-		System.out.println("Obtenir un producte");
-		Producte p=mapper.readValue(new URL("http://localhost:8080/ServeiREST/api/serveiCarro/obtenirProducte/"+token.getId()+"/65"),Producte.class );
-		System.out.println("Nom: "+p.getNom()+" Preu: "+p.getPreu()+" Disponibilitat: "+p.getDisponibilitat());
-		
-		
-		mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-		//mapper.configure(DeserializationFeature.UNWRAP_ROOT_VALUE, true);
-		//mapper.registerModule(new JaxbAnnotationModule());
-
-		Lllistaproductes llista=mapper.readValue(new URL("http://localhost:8080/ServeiREST/api/serveiCarro/obtenirProductes/"+token.getId()),Lllistaproductes.class  );
-		
-		List<Producte> productes=llista.getProductes();
-		
-		System.out.println("Obtenir tots els Productes");
-		int i=0;
-		for(Producte item: productes) {
-			System.out.println("---------");
-			System.out.println("Producte "+i);
-			System.out.println(item.getNom());
-			System.out.println(item.getPreu());
-			System.out.println(item.getDisponibilitat());
-			
-			i++;
-		}
-	//URL nova=	new URL("hh");
-	
-Lllistaproductes llista2=mapper.readValue(new URL("http://localhost:8080/ServeiREST/api/serveiCarro/obtenirProductes/data/"+token.getId()+"/12-03-2012/03-07-2014"),Lllistaproductes.class  );
-		
-		List<Producte> productes2=llista2.getProductes();
-		
-		System.out.println("Obtenir tots els Productes Filtrats per Data");
-		int i1=0;
-		for(Producte item: productes2) {
-			System.out.println("---------");
-			System.out.println("Producte "+i1);
-			System.out.println(item.getNom());
-			System.out.println(item.getPreu());
-			System.out.println(item.getDisponibilitat());
-			
-			i1++;
-		}
-		*/
 		
 		 ClientConfig clientConfig = new DefaultClientConfig();
 	        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
@@ -82,6 +34,13 @@ Lllistaproductes llista2=mapper.readValue(new URL("http://localhost:8080/ServeiR
 	        // GET request to findBook resource with a query parameter
 	        String getToken = "http://localhost:8080/ServeiREST/api/serveiCarro/Autenticar";
 	        String webResourceGet = client.resource(getToken).queryParam("nom", "admin").queryParam("passwd", "admin").accept(MediaType.APPLICATION_JSON).get(String.class);
+	        
+	       /* String getproductes = "http://localhost:8080/ServeiREST/api/serveiCarro/obtenirProductes";
+	        List<Producte> productes = client.resource(getproductes).queryParam("idUser", webResourceGet).accept(MediaType.APPLICATION_JSON).get(new GenericType<List<Producte>>(){});
+	        */
+	        
+	        String getproducte = "http://localhost:8080/ServeiREST/api/serveiCarro/obtenirProducte";
+	        Producte producte = client.resource(getproducte).queryParam("idUser", webResourceGet).queryParam("idProduct", "1").accept(MediaType.APPLICATION_JSON).get(Producte.class);
 	       /* ClientResponse response = webResourceGet.get(ClientResponse.class);
 	        String responseEntity = response.getEntity(String.class);
 
@@ -91,7 +50,14 @@ Lllistaproductes llista2=mapper.readValue(new URL("http://localhost:8080/ServeiR
 
 	        System.out.println(responseEntity.toString());
 	*/
-	        System.out.println("Token :"+webResourceGet);
+	        System.out.println("Token : "+webResourceGet);
+	        
+	        System.out.println("Nom: "+producte.getNom());
+	        
+	        /*for(Producte item: productes) {
+	        	System.out.println(item.getId());
+	        	System.out.println(item.getNom());
+	        }*/
 	}
 
 }
